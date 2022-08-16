bc1cf8f0fc8277ceda4554bc3c03e11643b973ff
迷渡 <justjavac@gmail.com>
2022-08-16 14:01:54 +0800

    browserslist
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,13 @@
 #![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
 
 #[tauri::command]
-fn greet(name: &str) -> String {
-  format!("Hello, {}!", name)
-}
-
-#[tauri::command]
-fn browser_list() -> Vec<i8> {
+fn browserslist() -> Vec<i8> {
   vec![1, 2, 3]
 }
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet, browser_list])
+    .invoke_handler(tauri::generate_handler![browserslist])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
