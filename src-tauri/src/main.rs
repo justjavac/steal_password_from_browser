7c7a2aee6019a448213b210e02ba356edb9cbc00
迷渡 <justjavac@gmail.com>
2022-08-17 14:57:58 +0800

    mock rust api
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,90 @@
 #![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
 
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Browser {
+  name: String,
+  installed: bool,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Password {
+  url: String,
+  username: String,
+  password: String,
+}
+
 #[tauri::command]
-fn browserslist() -> Vec<i8> {
-  vec![1, 2, 3]
+fn browserslist() -> Vec<Browser> {
+  vec![
+    Browser {
+      name: "Chrome".to_string(),
+      installed: true,
+    },
+    Browser {
+      name: "Chrome Dev".to_string(),
+      installed: true,
+    },
+    Browser {
+      name: "Chrome Beta".to_string(),
+      installed: false,
+    },
+    Browser {
+      name: "Chrome Canary".to_string(),
+      installed: true,
+    },
+    Browser {
+      name: "Edge".to_string(),
+      installed: true,
+    },
+    Browser {
+      name: "Edge Dev".to_string(),
+      installed: false,
+    },
+    Browser {
+      name: "Edge Beta".to_string(),
+      installed: false,
+    },
+    Browser {
+      name: "Edge Canary".to_string(),
+      installed: true,
+    },
+    Browser {
+      name: "Edge Dev Preview".to_string(),
+      installed: false,
+    },
+    Browser {
+      name: "Edge Preview".to_string(),
+      installed: false,
+    },
+  ]
+}
+
+#[tauri::command]
+fn passwordslist(browser: String) -> Vec<Password> {
+  vec![
+    Password {
+      url: "https://github.com".to_string(),
+      username: "admin".to_string(),
+      password: "12345678".to_string(),
+    },
+    Password {
+      url: "https://baidu.com".to_string(),
+      username: "root".to_string(),
+      password: "12345678".to_string(),
+    },
+    Password {
+      url: "https://google.com".to_string(),
+      username: "justjavac".to_string(),
+      password: "12345678".to_string(),
+    },
+  ]
 }
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![browserslist])
+    .invoke_handler(tauri::generate_handler![browserslist, passwordslist])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api";
+
+import { Browserslist } from "./Browserslist";
+import { type Password, Passwordslist } from "./Passwordslist";
 
 function App() {
+  const [passwords, setPasswords] = useState<Password[]>();
+
+  function handleClick(browser: string) {
+    invoke<Password[]>("passwordslist", { browser }).then(setPasswords);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen flex gap-2 divide-x divide-gray-700">
+      <Browserslist onClick={handleClick} />
+      {passwords == null ? <Empty /> : <Passwordslist passwords={passwords} />}
     </div>
   );
+}
+
+function Empty() {
+  return (
+    <div className="w-full h-full flex items-center justify-center text-red-400">
+      <h1>Click left to select a browser</h1>
+    </div>
+  )
 }
 
 export default App;
